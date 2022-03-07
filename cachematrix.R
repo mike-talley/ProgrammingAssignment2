7889bdfc8ec296458cb0a19a2d7f6a6a89d8c6cb
Mike Talley <81503342+mike-talley@users.noreply.github.com>
2022-03-06 21:46:57 -0800

    Update cachematrix.R
    My submission for this weeks assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming: Week 3 Programming Assignment
+## Goal: Use lexical scoping within functions to solve/invert a matrix, cache it, then retrieve it. 
 
-## Write a short comment describing this function
-
+## Function to hold / make the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setCache <- function(solve) m <<- solve
+  getCache <- function() m
+  list(set = set, get = get,
+       setCache = setCache,
+       getCache = getCache)
 }
 
-
-## Write a short comment describing this function
-
+## Function to solve(aka invert) a matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getCache()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setCache(m)
+  m
 }
+
+## Sample matrix from discussion board
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+
+## Place the above matrix into the first function
+aMatrix <- makeCacheMatrix(m1)
+
+## Solve(invert) matrix and cache it
+cacheSolve(aMatrix)
+
+## Retrieve the cached matrix
+cacheSolve(aMatrix)
